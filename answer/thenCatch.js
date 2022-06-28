8b78c98a00e741b94804621de273692a9385fea9
Patrizia-n <91250805+Patrizia-n@users.noreply.github.com>
2022-06-28 14:56:34 +1000

    Update thenCatch.js

--- answer/thenCatch.js
+++ answer/thenCatch.js
@@ -1,4 +1,4 @@
-// I added the promise to its own JS file and exported it like a Node environment export. However, this is not necessary. This code could simply be repeated in both files and that is fine too.
+
 let isRaining = true;
 
 const weather = new Promise((resolve, reject) => {
